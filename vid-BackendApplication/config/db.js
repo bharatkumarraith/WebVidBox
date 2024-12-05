b345086cb0d1dd0f6842d87c50bc47ae00e6d5bf
bharat_raithi <124795507+bharatkumarraith@users.noreply.github.com>
2024-12-05 14:17:29 +0530

    first commit

--- vid-BackendApplication/config/db.js
+++ vid-BackendApplication/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     });
     console.log('Database connected successfully');
   } catch (error) {
-    console.error('Database connection failedddddd:', error.message);
+    console.error('Database connection failed:', error.message);
     process.exit(1);
   }
 };
